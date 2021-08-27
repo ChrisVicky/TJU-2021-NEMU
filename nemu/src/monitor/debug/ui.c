fd708af57845dba0c0a64efec9641adb89ebbacd
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-27 14:53:05 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:53:05 up  6:05,  1 user,  load average: 0.41, 0.53, 0.51 8f32ef86e06be44ecf0bf80a230a08563339613

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -236,7 +236,7 @@ void ui_mainloop() {
 		char *str_end = str + strlen(str);
 		if(str==NULL) {
 			str = history_get(where_history())->line;
-			Log("Error str=>>%s<<" ,str);
+			//Log("Error str=>>%s<<" ,str);
 		}
 		/* extract the first token as the command */
 		char *cmd = strtok(str, " ");
