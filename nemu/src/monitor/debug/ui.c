b243984c1a03599c3cdea72216c874525e21ff80
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-27 09:29:05 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:29:05 up 41 min,  1 user,  load average: 0.57, 0.79, 0.66 10d5118767a79ac2ae3b9bbafeb965aef4b79caf

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -182,6 +182,7 @@ static int cmd_x(char* arg){
 static int cmd_p(char *args)
 {
 	bool flag = true;
+	printf("p %s\n" ,args);
 	int ans = expr(args, &flag);
 	if(!flag){
 		printf("Miss Match or Wrong Expression\n");
