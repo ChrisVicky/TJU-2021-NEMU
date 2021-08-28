00510935fa5067c47b796990e2467d3c050566e2
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-28 09:34:38 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:34:38 up  1:01,  1 user,  load average: 0.73, 0.55, 0.34 2d5ea2f4b5d59114d017ec735d56c5724bbb5d65

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -31,7 +31,8 @@ char* rl_gets() {
 		*/
 		int cnt = where_history();
 		printf("\033[40;36m Now History: %d\n\033[0m" ,cnt);
-		line_read = history_get(cnt)->line;	
+		line_read = history_get(cnt)->line;
+		printf("\033[40;35m New line_read: %s\n\033[0m" ,line_read);
 	}
 	return line_read;
 }
