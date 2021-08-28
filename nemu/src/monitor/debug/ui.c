a2201577293a85632a3b25484e975349e78f3e34
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-28 09:32:30 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:32:30 up 59 min,  1 user,  load average: 0.36, 0.48, 0.30 ce71a592d696a4b4b48e317b3002cf526ee3fc0e

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -23,10 +23,14 @@ char* rl_gets() {
 	if (line_read && *line_read) {
 		add_history(line_read);
 	}else{
-		HIST_ENTRY *hist = current_history();
+
+		/*HIST_ENTRY *hist = current_history();
 		line_read = hist->line;
 		printf("\033[40;37m NULL EMPTY\n\033[0m");
-		printf("\033[40;37m line_read(previous) '%s'\n\033[0m" ,line_read);	
+		printf("\033[40;37m line_read(previous) '%s'\n\033[0m" ,line_read);
+		*/
+		int cnt = where_history();
+		printf("\033[40;36m Now History: %d\n\033[0m" ,cnt);	
 	}
 	return line_read;
 }
