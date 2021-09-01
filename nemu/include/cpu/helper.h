f163bf743ad94386fcaa4ec4bf4110768af9a9f8
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-01 16:12:20 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:12:20 up  3:31,  1 user,  load average: 0.69, 0.60, 0.68 38849fe315a04d2a151653d67fca47f6999bf07c

--- nemu/include/cpu/helper.h
+++ nemu/include/cpu/helper.h
@@ -8,6 +8,7 @@
 /* All function defined with 'make_helper' return the length of the operation. */
 #define make_helper(name) int name(swaddr_t eip)
 
+/* Instruction Fetched */
 static inline uint32_t instr_fetch(swaddr_t addr, size_t len) {
 	return swaddr_read(addr, len);
 }
