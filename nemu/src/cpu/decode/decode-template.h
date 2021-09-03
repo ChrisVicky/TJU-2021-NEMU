9b140c459d58dfefb5420516ba6a37fb15bf82f4
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 09:41:17 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:41:17 up 41 min,  1 user,  load average: 0.92, 0.83, 0.72 7a098811c6c86047948e0ab398fe11d748cedff

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -25,14 +25,14 @@ make_helper(concat(decode_i_, SUFFIX)) {
 /* sign immediate */
 make_helper(concat(decode_si_, SUFFIX)) {
 	op_src->type = OP_TYPE_IMM;
-
+	op_src->val = instr_fetch(eip, DATA_BYTE);
 	/* TODO: Use instr_fetch() to read `DATA_BYTE' bytes of memory pointed
 	 * by `eip'. Interpret the result as an signed immediate, and assign
 	 * it to op_src->simm.
 	 *
 	op_src->simm = ???
 	 */
-	panic("please implement me");
+//	panic("please implement me");
 
 	op_src->val = op_src->simm;
 
