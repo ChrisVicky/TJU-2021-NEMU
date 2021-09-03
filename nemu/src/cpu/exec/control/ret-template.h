f1b18a1000ba5fe10b96c81579271ace6ba81b64
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 12:21:26 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:21:26 up  3:21,  1 user,  load average: 0.59, 0.48, 0.50 fee4a140894f05ff289fffa0b4f03671a189fb20

---
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -0,0 +1,16 @@
+#include "cpu/exec/template-start.h"
+
+#define instr ret
+
+#if DATA_BYTE == 4
+make_helper(ret){
+    Log("RET");
+    int instruct = swaddr_read(cpu.esp, 4);
+    cpu.esp += 4;
+    cpu.eip = instruct;
+    print_asm(str(instr));
+    return 0;
+}
+#endif
+
+#include "cpu/exec/template-end.h"
