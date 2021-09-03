76e50e015cf9fa8f13bb21b58e6ed0f39589ab10
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 09:54:44 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:54:44 up 55 min,  1 user,  load average: 0.39, 0.70, 0.72 1d75ef6738c9a7738767e1ea4204823d8763f42

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -11,7 +11,7 @@ make_helper(concat(call_si_, SUFFIX)) {
     
     print_asm(str(instr) " *%s", op_src->str);
     cpu.eip = cpu.eip + op_src->val - (len + 1);
-    Log("cpu.eip = %d, len = %d" ,cpu.eip, len);
+    Log("cpu.eip = %x, len = %d" ,cpu.eip, len);
     return len + 1;
 }
 #endif
