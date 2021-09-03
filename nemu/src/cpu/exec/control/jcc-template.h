2ffddbf741bf0eb87795587b7cc26353ce85ff83
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 20:05:51 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:05:51 up  7:09,  1 user,  load average: 0.74, 0.61, 0.55 e5106ad04cc8ffa2b5fdd638eb3166e47dd7cc0

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -1,9 +1,55 @@
 #include "cpu/exec/template-start.h"
+#define CF cpu.eflags.CF
+#define PF cpu.eflags.PF
+#define AF cpu.eflags.AF
+#define ZF cpu.eflags.ZF
+#define SF cpu.eflags.SF
+#define TF cpu.eflags.TF
+#define IF cpu.eflags.IF
+#define DF cpu.eflags.DF
+#define OF cpu.eflags.OF
+#define IOPF cpu.eflags.IOPL
+#define NT cpu.eflags.NT
+#define CX cpu.gpr[1]._16
+#define ECX cpu.gpr[1]._32
+
 #ifndef __IS_H__
 #define __IS_H__
-bool is_je() { return cpu.eflags.ZF == 1; }
-
+bool is_ja() { return (!CF) && (!ZF); }
+bool is_jae() { return (!CF); }
+bool is_jb() { return CF==1; }
+bool is_jbe() { return CF==1 || ZF == 1; }
+bool is_jc() { return CF == 1; }
+bool is_jcxz() { return !CX; }
+bool is_jecxz() { return !ECX; }
+bool is_je() { return ZF == 1; }
+bool is_jz() { return ZF == 1; }
+bool is_jg() { return !ZF && SF==OF; }
+bool is_jge() { return SF==OF; }
+bool is_jl() { return SF!=OF; }
+bool is_jle() { return ZF==1 && SF!=OF; }
+bool is_jna() { return CF==1 && ZF==1; }
+bool is_jnae() { return CF==1; }
+bool is_jnb() { return !CF; }
+bool is_jnbe() { return CF==0 && ZF==0; }
+bool is_jnc() { return !CF; }
+bool is_jne() { return !ZF; }
+bool is_jng() { return ZF==1 || SF!=OF; }
+bool is_jnge() { return SF!=OF; }
+bool is_jnl() { return SF==OF; }
+bool is_jnle() { return !ZF && SF==OF; }
+bool is_jno() { return !OF; }
+bool is_jnp() { return !PF; }
+bool is_jns() { return !SF; }
+bool is_jnz() { return !ZF; }
+bool is_jo() { return OF==1; }
+bool is_jp() { return PF==1; }
+bool is_jpe() { return PF==1; }
+bool is_jpo() { return !PF; }
+bool is_js() { return SF==1; }
+// bool is_jz() { return ZF==1; }
 #endif
+
 static void do_execute(){
     cpu.eip = op_src->val;
 #if DATA_BYTE == 2
@@ -32,3 +78,17 @@ make_helper(concat3(instr, _si_, SUFFIX)) {
 make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
+/*
+#undef __IS_H__
+#undef CF
+#undef PF
+#undef AF
+#undef ZF
+#undef SF
+#undef TF
+#undef IF
+#undef DF
+#undef OF 
+#undef IOPF 
+#undef NT
+*/
