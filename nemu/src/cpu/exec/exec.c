0120d52a31d8d6935521d12b87e7066273b846ca
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 12:11:24 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:11:24 up  3:11,  1 user,  load average: 0.49, 0.39, 0.53 14b2a392a434aa05f2414bca148cc02aababefa2

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -29,8 +29,8 @@ make_group(group1_v,
 
 /* 0x83 */
 make_group(group1_sx_v,
-	cmp_si2rm_v, or_si2rm_v, inv, inv, 
-	and_si2rm_v, sub_si2rm_v, inv, inv)
+	inv, or_si2rm_v, inv, inv, 
+	and_si2rm_v, sub_si2rm_v, inv, cmp_si2rm_v)
 
 /* 0xc0 */
 make_group(group2_i_b,
