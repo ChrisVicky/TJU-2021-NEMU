0968bdc22aeb9c9dd6bd6171815d131cc24e160c
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 22:45:03 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:45:03 up  9:48,  1 user,  load average: 0.45, 0.45, 0.49 89348542688cee3f3bd4d8de1a5370d0aa8f9ed

--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -1,5 +1,103 @@
 #include "cpu/exec/helper.h"
 
+#define instr ja
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jae
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jb
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jbe
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jc
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jcxz
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jecxz
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
 #define instr je
 #define DATA_BYTE 1
 #include "jcc-template.h"
@@ -14,6 +112,88 @@
 #undef DATA_BYTE
 #undef instr
 
+#define instr jz
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jg
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jge
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jl
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
+
+#define instr jle
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
 
 
+#define instr jna
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+#undef instr
 
