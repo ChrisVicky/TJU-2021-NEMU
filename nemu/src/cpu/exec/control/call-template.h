fa0d0dc10b4413dd1d0e58810c353d0c3d820b33
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 13:08:10 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:08:10 up 12 min,  1 user,  load average: 0.66, 0.76, 0.51 89ed4e90408aa9fc49f30aa4d6beef23b4d6e0ec

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -11,7 +11,6 @@ make_helper(concat(call_si_, SUFFIX)) {
     
     cpu.eip = cpu.eip + op_src->val;
     print_asm(str(instr) " *%x", cpu.eip + len + 1);
-    Log("cpu.eip = %x, len = %d, val = %x" ,cpu.eip, len, op_src->val);
     return len + 1;
 }
 #endif
@@ -23,7 +22,6 @@ make_helper(concat(call_si_, SUFFIX)) {
 #endif
 
 make_helper(concat(call_rm_, SUFFIX)) {
-    Log("RM");
     int len = concat(decode_rm_, SUFFIX) (eip + 1);
     cpu.esp -= 4;
     swaddr_write(cpu.esp, 4, cpu.eip + len + 1);
