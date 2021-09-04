17ebba1aad5ba5a25cbc6fbe6bbfdd9e62850ce8
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 13:17:20 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:17:20 up  1:40,  1 user,  load average: 0.87, 0.66, 0.57 5f8e575865d2f61a6c3e92a6a6b75a7b91d8e1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -293,7 +293,7 @@ static uint32_t exe(int q, int p, bool *flag)
 				break;
 			default:
 				data = cpu.gpr[position]._32;
-				Log("Reg %s; data=%x	position=%d" ,tokens[p].str ,data ,position);
+				Log("Reg %s; data=%x; temp=%s;p osition=%d" ,tokens[p].str ,data ,temp ,position);
 				break;
 			}
 			break;
