68f376b8f3dc967e0cce2fe667bbbb22360e87d6
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-06 18:11:48 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:11:48 up  6:44,  1 user,  load average: 0.86, 0.67, 0.42 f348bb82240c6da7ea253a716383dc4ce3839f7

--- nemu/src/memory/dram.c
+++ nemu/src/memory/dram.c
@@ -52,11 +52,8 @@ void init_ddr3() {
 }
 
 static void ddr3_read(hwaddr_t addr, void *data) {
-	// Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
-	if(addr >= HW_MEM_SIZE){
-		printf("\33[1;31mphysical address %x is outside of the physical memory!\33[0m\n", addr);
-		return ;
-	}
+	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
+	
 	dram_addr temp;
 	temp.addr = addr & ~BURST_MASK;
 	uint32_t rank = temp.rank;
@@ -77,7 +74,6 @@ static void ddr3_read(hwaddr_t addr, void *data) {
 
 static void ddr3_write(hwaddr_t addr, void *data, uint8_t *mask) {
 	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
-
 	dram_addr temp;
 	temp.addr = addr & ~BURST_MASK;
 	uint32_t rank = temp.rank;
