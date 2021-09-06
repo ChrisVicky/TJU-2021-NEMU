f94ef2e7821f510af02e768406b87373d72b4f80
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-06 16:55:20 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:55:20 up  5:28,  1 user,  load average: 0.49, 0.44, 0.46 1aca8faf8ff3263b36b1d1eeec9888b6bc7a3af7

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -3,10 +3,19 @@
 #define instr cmp
 
 static void do_execute() {
-    DATA_TYPE_S result = (DATA_TYPE_S) op_dest->val - (DATA_TYPE_S) op_src->val;
+//    DATA_TYPE_S result = (DATA_TYPE_S) op_dest->val - (DATA_TYPE_S) op_src->val;
     // Log("%x - %x = %d" ,op_dest->val, op_src->val, result);
-    update_eflags_pf_zf_sf(result);
-    update_eflags_cf_of_MINUS(result, op_dest->val, op_src->val);
+//    update_eflags_pf_zf_sf(result);
+//    cpu.eflags.CF = result > dest;
+//	cpu.eflags.OF = MSB((dest ^ src) & (dest ^ result));
+
+    DATA_TYPE dest = op_dest->val;
+	DATA_TYPE src = op_src->val;
+	DATA_TYPE result = dest - src;
+
+	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = result > dest;
+	cpu.eflags.OF = MSB((dest ^ src) & (dest ^ result));
     // Log("Show Flags : ZF = %x, OF = %x, SF = %x" ,cpu.eflags.ZF, cpu.eflags.OF, cpu.eflags.SF);
     Log("Compare 0x%d and 0x%d, RESULT=0x%d\n" ,op_dest->val, op_src->val, result);
     print_asm_template2();
