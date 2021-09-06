c28bcb289abe1afb985be99328b618a218f9a672
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-07 00:01:49 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:01:49 up 12:34,  1 user,  load average: 1.92, 1.30, 1.08 fe9bc132b17842fe2e0a67b45689c15e5ace647b

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -4,7 +4,8 @@
 
 make_helper(concat(ret_, SUFFIX)) {
     cpu.eip = MEM_R(REG(R_ESP));
-    REG(R_ESP) += DATA_BYTE;
+    //REG(R_ESP) += DATA_BYTE;
+    cpu.esp += DATA_BYTE;
 #if DATA_BYTE == 2
     cpu.eip = cpu.eip & 0x0000ffff;
 #endif
@@ -16,7 +17,8 @@ make_helper(concat(ret_, SUFFIX)) {
 make_helper(concat(ret_i_, SUFFIX)){
     int len = decode_i_w(eip + 1);
     cpu.eip = MEM_R(REG(R_ESP));
-    REG(R_ESP) += DATA_BYTE;
+//    REG(R_ESP) += DATA_BYTE;
+    cpu.esp += DATA_BYTE;
 #if DATA_BYTE == 2
     cpu.eip = cpu.eip & 0x0000ffff;
 #endif
