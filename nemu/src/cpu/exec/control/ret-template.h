4cc077db6e4ba6d1dc2797e3e29dc88a55f58d37
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-06 16:04:19 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:04:19 up  4:37,  1 user,  load average: 0.81, 0.63, 0.50 45f3c49d5f98aa6b475503b162578bc9ecde084c

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -2,14 +2,24 @@
 
 #define instr ret
 
-#if DATA_BYTE == 4
 make_helper(ret){
     int instruct = swaddr_read(cpu.esp, 4);
     cpu.esp += 4;
     cpu.eip = instruct;
-    print_asm(str(instr));
-    return 0;
+    cpu.eip -= 1;
+    print_asm_template1();
+    return 1;
 }
-#endif
+
+make_helper(concat(instr, _i)){
+    int len = decode_i_w(eip + 1);
+    int instruct = swaddr_read(REG(R_ESP), 4);
+    REG(R_ESP) += 4;
+    cpu.eip = instruct;
+    cpu.eip -= (len + 1);
+    REG(R_ESP) += op_src->val;
+    print_asm_template2();
+    return len + 1;
+}
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -1,13 +1,5 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "ret-template.h"
-#undef DATA_BYTE
-
-#define DATA_BYTE 2
-#include "ret-template.h"
-#undef DATA_BYTE
-
 #define DATA_BYTE 4
 #include "ret-template.h"
 #undef DATA_BYTE
