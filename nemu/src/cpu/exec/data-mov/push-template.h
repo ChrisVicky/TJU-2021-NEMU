796847fd1c04e7229db54c1604e84ca860521b96
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-06 23:53:49 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:53:49 up 12:26,  1 user,  load average: 1.75, 1.14, 0.94 28e8cee5b3785fec1a791c9a291e7e6b9d967f6

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -2,19 +2,27 @@
 
 #define instr push
 
+#if DATA_BYTE == 2 || DATA_BYTE == 4
 static void do_execute(){
-    cpu.esp -= DATA_BYTE;
+    REG(R_ESP) -= DATA_BYTE;
     MEM_W(cpu.esp, op_src->val);
     Log("cpu.esp = 0x%x" ,cpu.esp);
     Log("REG(R_ESP) = 0x%x" ,REG(R_ESP));
     print_asm_template1();
 }
 
-make_instr_helper(i)
+// make_instr_helper(i)
 
-#if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(m)
 make_instr_helper(r)
 #endif
+
+make_helper(concat(push_i_, SUFFIX)){
+    int len = concat(decode_i_, SUFFIX)(eip + 1);
+    cpu.esp -= DATA_BYTE;
+    MEM_W(cpu.esp, op_src->val);
+    print_asm_template1();
+    return len + 1;
+}
 
 #include "cpu/exec/template-end.h"
