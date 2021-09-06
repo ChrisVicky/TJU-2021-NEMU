da2b6c14727ec753543d32a19073907259bd68d3
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-07 00:02:40 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:02:40 up 12:35,  1 user,  load average: 1.65, 1.32, 1.10 8c4b2efe8d808694955c4a07b909d87f66899f8

--- nemu/src/cpu/exec/data-mov/lods-template.h
+++ nemu/src/cpu/exec/data-mov/lods-template.h
@@ -4,7 +4,8 @@
 
 make_helper(concat3(instr, _m_, SUFFIX)){
     REG(R_EAX) = MEM_R(cpu.esi);
-    REG(R_ESI) += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
+    //REG(R_ESI) += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
+    cpu.esi += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
     print_asm("lods" str(SUFFIX) " %%ds:(%%esi),%%%s", REG_NAME(R_EAX));
     return 1;
 }
