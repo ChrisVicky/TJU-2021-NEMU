c92f92811a457b5bcc7fed340e82fccb355d4c39
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-06 22:22:52 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:22:52 up 10:55,  1 user,  load average: 0.84, 0.76, 0.72 1406b811ed1ecca2c857f3715db1918250a9f74

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -2,24 +2,31 @@
 
 #define instr ret
 
-make_helper(ret){
-    int instruct = swaddr_read(cpu.esp, 4);
-    cpu.esp += 4;
+make_helper(concat(ret_, SUFFIX)) {
+    int instruct = MEM_R(REG(R_ESP));
+    cpu.esp += DATA_BYTE;
     cpu.eip = instruct;
+#if DATA_BYTE == 2
+    cpu.eip = cpu.eip & 0x0000ffff;
+#endif
     cpu.eip -= 1;
     print_asm_template1();
     return 1;
 }
 
-make_helper(concat(instr, _i)){
+make_helper(concat(ret_i_, SUFFIX)){
     int len = decode_i_w(eip + 1);
-    int instruct = swaddr_read(REG(R_ESP), 4);
-    REG(R_ESP) += 4;
+    int instruct = MEM_R(REG(R_ESP));
+    REG(R_ESP) += DATA_BYTE;
     cpu.eip = instruct;
-    cpu.eip -= (len + 1);
+#if DATA_BYTE == 2
+    cpu.eip = cpu.eip & 0x0000ffff;
+#endif
     REG(R_ESP) += op_src->val;
     print_asm_template2();
+    cpu.eip -= (len + 1);
     return len + 1;
 }
+
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -1,5 +1,12 @@
 #include "cpu/exec/helper.h"
 
+#define DATA_BYTE 2
+#include "ret-template.h"
+#undef DATA_BYTE
+
 #define DATA_BYTE 4
 #include "ret-template.h"
 #undef DATA_BYTE
+
+make_helper_v(ret_i)
+make_helper_v(ret)
