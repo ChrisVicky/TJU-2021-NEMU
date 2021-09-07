2ecc66fe5327e649fcadea05968310acd4006ae5
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-07 23:13:11 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:13:11 up  3:47,  1 user,  load average: 1.97, 1.71, 1.38 d46871d4930017042fbd73bd77bb95641134133f

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -62,7 +62,8 @@ FLOAT f2F(float a) {
 	ret += (1<<23);
 	unsigned int exp = ((temp >> 23) & 0xff) - BIAS;
 	
-	if(exp==0){
+	if(exp==0){	
+		if(temp >> 31) ret = ~ ret + 1;
 		return ret;
 	}
 	switch((exp - 7) > 0){
