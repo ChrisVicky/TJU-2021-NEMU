24de732ab226645712e027efa71796a01a7ca5be
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-08 14:48:59 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:48:59 up  1:03,  1 user,  load average: 0.67, 0.54, 0.56 2a8ca1bf27dd02233d6a7062d1e9352b1e2ddf23

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -86,7 +86,7 @@ void load_elf_variables(){
 	printf("name	info\n");
 	for(i=0;i<nr_symtab_entry;i++){
 		if(symtab[i].st_info==17)
-			printf("%s	%x\n" ,strtab+symtab[i].st_name ,symtab[i].st_info);
+			printf("%s	%d\n" ,strtab+symtab[i].st_name ,symtab[i].st_info);
 	}
 	return ;
 }
