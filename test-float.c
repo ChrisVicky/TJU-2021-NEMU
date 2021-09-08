83cd0f3b149497c61c0af2b381d5705cf0a88137
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-08 10:39:04 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:39:04 up  1:23,  1 user,  load average: 0.72, 0.70, 0.67 f2900d8e91786c96322b26d510b665de11d6edb

--- test-float.c
+++ test-float.c
@@ -11,6 +11,7 @@ int f2F(float a) {
 	 * performing arithmetic operations on it directly?
 	 */
 	/* float is a 32-bit 1, 8, 23 bits structure variable */
+/*
 	unsigned int temp = ((unsigned int *) & a) [0];
 	const int BIAS = 127;
 	FLOAT ret = temp & 0x7fffff;
@@ -34,12 +35,30 @@ int f2F(float a) {
 			ret = ret << (exp - 7);
 			break;
 	}
-*/
+
 	if(temp >> 31) ret = ~ ret + 1;
-	return ret;
+	return ret;*/
+	int u[100];
+	memcpy((void *)u,(void *)&a,4);
+	int tep=u[0];
+	int sign = tep >> 31;
+  int exp = (tep >> 23) & 0xff;
+  int x = tep & 0x7fffff;
+  if (exp != 0)
+    x += 1 << 23;
+  exp -= 150;
+  if (exp < -16)
+    x >>= -16 - exp;
+  if (exp > -16)
+    x <<= 16 + exp;
+  return sign == 0 ? x : -x;
 }
 int main(){
 	int t = f2F(1.25);
+	FLOAT _FLOAT_q = f2F(0.0001);
+	printf("%x\n" ,_FLOAT_q);
+	FLOAT _FLOAT_a = f2F(1e-4);
+	printf("%x\n" ,_FLOAT_a);
     FLOAT _FLOAT_b = f2F(0.551222);
 	printf("%x\n" ,_FLOAT_b);
 	FLOAT _FLOAT_c = f2F(-1.0);
