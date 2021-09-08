83f73faf80e3f3adf0ffeb6465dd0337caf2f45a
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-08 11:11:49 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:11:49 up  1:56,  1 user,  load average: 1.39, 1.16, 0.98 13c374ccd49a6e8775e606fde6e7e2f35f17f43d

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -33,13 +33,38 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 				a->%eax, d->%edx, r->registers
 				divl %1 %0 : %0 = %0 / %1
 	*/
+/*
 	asm volatile (
 		"divl %1, %0;"
 		"shll %2;"
 		: "=a" (a), "=d"(b)
 		: "r" (16), "a"(a), "d"(b)
-	);
-	
+	);*/
+	int sign=1;
+	if(a<0)
+	{
+		a=-a;
+		sign*=-1;
+	}
+	if(b<0)
+	{
+		b=-b;
+		sign*=-1;
+	}
+	FLOAT ans=a/b;
+	int i;
+	a=a%b;
+	for(i=1;i<=16;i++)
+	{
+		a<<=1;
+		ans<<=1;
+		if(a>=b)
+		{
+			a-=b;
+			ans++;
+		}
+	}
+	return ans*sign;	
 
 // 	nemu_assert(0);
 	return a;
