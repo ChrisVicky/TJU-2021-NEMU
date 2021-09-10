b64f481ebebcfce72cdd85842b7a0a25ea573e54
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-10 15:52:01 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:52:01 up  2:04,  1 user,  load average: 1.35, 1.06, 0.82 7093ac5ed75eee17f9a175fd9155944ac31cf092

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -11,7 +11,7 @@
 /* IF_DEBUG==1 时启动ui_loop, 否则直接执行cpu_exec(-1); */
 #define IF_DEBUG 0
 
-#define ERROR(format, ...) printf("\33[1;33m" format "\33[0m\n",## __VA_ARGS__)
+#define SUCCESS(format, ...) printf("\33[1;33m" format "\33[0m\n",## __VA_ARGS__)
 void cpu_exec(uint32_t);
 const char *register_name[] = {"eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi"};
 /* We use the `readline' library to provide more flexibility to read from stdin. */
@@ -344,7 +344,7 @@ void ui_mainloop()
 {
 #if IF_DEBUG == 0
 	
-	ERROR("TEST %s" ,"TET");
+	SUCCESS("NO %s" ,"DEBUG");
 	cpu_exec(-1);
 	return ;
 #endif
