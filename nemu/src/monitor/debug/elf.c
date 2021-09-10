e46097d3a5c73ab7299aadc40b5bfbfab401d59d
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-10 15:33:28 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:33:28 up  1:45,  1 user,  load average: 1.10, 0.69, 0.59 96db546f66ff2d2e2721c87b50cd7fb3af7d9a7

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -12,8 +12,7 @@ void load_elf_tables(int argc, char *argv[]) {
 	int ret;
 	Assert(argc == 2, "run NEMU with format 'nemu [program]'");
 	exec_file = argv[1];
-	Log("PRESENT EXE_FILE");
-	printf("exec_file=%s\n" ,exec_file);
+	printf("\33[1;32mexec_file=%s\33[0m\n" ,exec_file);
 
 	FILE *fp = fopen(exec_file, "rb");
 	Assert(fp, "Can not open '%s'", exec_file);
