0eccee2a3aef549e837f37103f6ce00019926c66
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-10 16:39:31 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:39:31 up  2:51,  1 user,  load average: 0.61, 0.57, 0.62 3e6eb0e8513fab7f89bb006b3be78c9af7febee

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -126,7 +126,7 @@ char * get_func_name_by_address(int value){
 /* #define STT_FUNC	2		Symbol is a code object */ 
 	for(i=0;i<nr_symtab_entry;i++){
 		Log("other = 0x%x	name=%s" ,symtab[i].st_info & 0xf, strtab + symtab[i].st_name);
-		if(((symtab[i].st_other & 0xf ) != 0x2)/* ||symtab[i].st_value > value*/) continue;
+		if(((symtab[i].st_info & 0xf ) != 0x2)/* ||symtab[i].st_value > value*/) continue;
 		Log("EDD");
 		if(value - symtab[i].st_value < temp){
 			Log("HIT");
