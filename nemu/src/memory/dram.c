d201676a4738d60da12d7be511b5b922e82bc13c
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-11 12:49:37 +0000

    >  compile NEMU 3020202184 simpleedu Linux simpleedu 4.15.0-154-generic #161-Ubuntu SMP Fri Jul 30 13:04:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:49:37 up 4 days,  4:42,  1 user,  load average: 0.32, 0.18, 0.07 51c344ec65a0c8c02676231cda5753d0d3fcf9ec

--- nemu/src/memory/dram.c
+++ nemu/src/memory/dram.c
@@ -53,7 +53,6 @@ void init_ddr3() {
 
 static void ddr3_read(hwaddr_t addr, void *data) {
 	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
-	
 	dram_addr temp;
 	temp.addr = addr & ~BURST_MASK;
 	uint32_t rank = temp.rank;
