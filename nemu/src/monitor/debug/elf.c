e3502914821e13b7caa242c7dbbbe22472cdf9b3
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-11 22:34:27 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:34:27 up  1:51,  1 user,  load average: 0.94, 0.69, 0.58 4f60e92f78d13d3bc70336c43115bd462fc7bfb3

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -124,9 +124,7 @@ char * get_func_name_by_address(int value){
 	int ret = 0;
 	for(i=0;i<nr_symtab_entry;i++){
 		int type = symtab[i].st_info & 0xf;
-		Log("type = 0x%x	name = %s" ,type ,strtab + symtab[i].st_name);
-		if(type != 0x2 && type != 0x0 ) continue;
-		//if(symtab[i].st_value > value)
+		if((type != 0x2 && type != 0x0) || symtab[i].st_value > value) continue;
 		//Log("HIT");
 		if(value - symtab[i].st_value < temp){
 			Log("HIeeT");
