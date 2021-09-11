999a8cc8f27d6f71aa89d15f90ae33d602e56974
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-11 22:54:14 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:54:14 up  2:11,  1 user,  load average: 0.89, 0.70, 0.62 53acfe968c0c830c936bad2ece01e30a7c37f0e1

--- nemu/src/monitor/debug/ui_impl.h
+++ nemu/src/monitor/debug/ui_impl.h
@@ -364,8 +364,8 @@ static int cmd_bt(char * args){
 		int offset = 1, address=0;
 		char * func_name = get_func_name_by_address(eip, &offset, &address);
 		SUCCESS("FUNC NAME\n");
-		if(strlen(func_name)==0) PRINT("Unknown Func Name\n");
-		else  PRINT("0x%08x<%s>\n" ,address ,func_name);
+		if(strlen(func_name)==0) PRINT("0x%08x Unknown Func Name\n", address);
+		else  PRINT("0x%08x <%s>\n" ,address ,func_name);
 		SUCCESS("%-12s%-12s\n" ,"ebp" ,"esp");
 		PRINT("0x%-10x0x%-10x\n" ,ebp, esp);
 		int i,x, temp_ebp=ebp;
