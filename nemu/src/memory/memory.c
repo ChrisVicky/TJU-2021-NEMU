01c1ba6c83d589da8fe0317a999187b9d8a005a0
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-14 20:53:54 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:53:54 up  2:57,  1 user,  load average: 0.68, 0.49, 0.62 242ee26930433dbd115c40be994c61891fa1ce87

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,7 +1,7 @@
 #include "common.h"
 #include "cache.h"
-uint32_t dram_read(hwaddr_t, size_t);
-void dram_write(hwaddr_t, size_t, uint32_t);
+//uint32_t dram_read(hwaddr_t, size_t);
+//void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
 
@@ -10,7 +10,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	printf("WER\n");
 	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
 	*/
-	 return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 	/* 	~0u : ~unsigned int 0 = 0xffff;
 	 *	取低地址的数据：如要取全部，应使得len = 4;
 		len = 1 : 右移 3 字节 取高 1 位
