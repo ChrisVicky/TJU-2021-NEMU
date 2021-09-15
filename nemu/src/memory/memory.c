725df6dee4b42744ef419f2fa4c64e4caceefbee
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-15 15:39:29 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:39:29 up 11 min,  1 user,  load average: 1.26, 1.13, 0.69 5d462e1f43befa0da8a2b2fecabe6cbe8b0df66

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -10,10 +10,10 @@ int cache_read(int, int);
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 
-	unsigned int cache = cache_read(addr, len) & (~0u >> ((4 - len) << 3));
-	// unsigned int dram = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	// unsigned int cache = cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	unsigned int dram = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 	// printf("address = %x\tlen = %x\tcache = %x\tdram = %x\n" ,addr, (int)len,cache, dram);
-	return cache;
+	return dram;
 		/* 	~0u : ~unsigned int 0 = 0xffff;
 	 *	取低地址的数据：如要取全部，应使得len = 4;
 		len = 1 : 右移 3 字节 取高 1 位
@@ -24,14 +24,14 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	//printf("hwaddr_write\n");
-	/*
+	
 	int i;
 	for(i=0;i<len;i++){
 		int temp_data = (data>>i) & 0xff;
 		cache_write(addr+i, temp_data);
 	}
-	*/
-	dram_write(addr, len, data);
+	
+	// dram_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
