2a8c01249a3881a024c21e3cee3991b5b8df741c
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-15 18:52:15 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:52:15 up  3:24,  1 user,  load average: 0.98, 0.84, 0.86 8d46854ddab23566229b2cc6b6a4a7323205937b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -30,7 +30,8 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	cache_write(addr, len, data);
+	// cache_write(addr, len, data);
+	dram_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
