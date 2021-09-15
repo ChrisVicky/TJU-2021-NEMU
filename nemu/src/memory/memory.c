41460d11bb302155105b9dc1b27f39c6a1280e25
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-15 17:37:41 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:37:41 up  2:09,  1 user,  load average: 1.22, 1.05, 0.90 ea47b69b62240a19ea35398b19e10d775003021d

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -18,7 +18,8 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 		nemu_state = 0;
 	}
 	//nemu_state = 0;
-	return cache;
+//	return cache;
+	return dram;
 		/* 	~0u : ~unsigned int 0 = 0xffff;
 	 *	取低地址的数据：如要取全部，应使得len = 4;
 		len = 1 : 右移 3 字节 取高 1 位
@@ -29,14 +30,14 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	//printf("hwaddr_write\n");
-	/*
+	
 	int i;
 	for(i=0;i<len;i++){
 		int temp_data = (data>>i) & 0xff;
 		cache_write(addr+i, temp_data);
 	}
-	*/
-	dram_write(addr, len, data);
+	
+	//dram_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
