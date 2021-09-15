034263ad73dd4770a1b1739a7499bafb128893df
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-15 19:01:44 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:01:44 up  3:33,  1 user,  load average: 1.98, 1.58, 1.21 6bae3239f13dac22cf9f95317fdc1271ccb71fce

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -49,8 +49,8 @@ static void write(int addr, int content){
             return;
         }
     }
+    /* No free cache -> random choose a cache to set. */
     srand((unsigned)time(NULL));
-    //printf("RANDOM\n");
     int line = rand()%7;
     for(block_offset=0;block_offset<64;block_offset++){
         cache.set[set_offset][line].block[block_offset] = dram_read(make_addr(tag, set_offset, block_offset), 1);
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -13,23 +13,13 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 
 	unsigned int cache = cache_read(addr, len) & (~0u >> ((4 - len) << 3));
 	return cache;
-	/*if(dram!=cache){
-		printf("address = %x\tlen = %x\tcache = %x\tdram = %x\n" ,addr, (int)len,cache, dram);
-		nemu_state = 0;
-	}*/
-	
-	//nemu_state = 0;
-	/*
-	unsigned int dram = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	return dram;
-	*/
-	//return ;
-		/* 	~0u : ~unsigned int 0 = 0xffff;
+	/* return dram_read(addr, len) & (~0u >> ((4 - len) << 3)); */
+	/* 	~0u : ~unsigned int 0 = 0xffff;
 	 *	取低地址的数据：如要取全部，应使得len = 4;
 		len = 1 : 右移 3 字节 取高 1 位
 		len = 2 : 右移 2 字节 取高 2 位
 		len = 4 : 右移 0 字节 取 4 位
-	 */
+	*/
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
