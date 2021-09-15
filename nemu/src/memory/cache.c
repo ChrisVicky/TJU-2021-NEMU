8de44478fc32fd41185ad0f0ba042c1e7d3a168f
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-15 16:22:25 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:22:25 up 54 min,  1 user,  load average: 1.00, 0.82, 0.83 9f035fd25334cd89ddeb327f1eb05dc2275558ca

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -136,6 +136,7 @@ int cache_read(int address, int len){
     for(i=0;i<len;i++){
         char temp = cache.read(&cache, address+i) & 0xff;    
         ret += temp<<(i*8);  
+        printf("ret = %08x\n" ,ret);
     }
 //    int ret1 = dram_read(address, len);
    // printf("0x08%x 0x%x 0x%x\n" ,address,ret,ret1);
