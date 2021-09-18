b9515a4970a901319262ccbd1023b292f53f733c
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-18 17:54:43 +0000

    >  compile NEMU 3020202184 simpleedu Linux simpleedu 4.15.0-154-generic #161-Ubuntu SMP Fri Jul 30 13:04:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:54:43 up 11 days,  9:47,  1 user,  load average: 0.11, 0.06, 0.01 d22b16356e4c9b6f1943ac6404525a8e5bd4b03

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -28,9 +28,9 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
   int i;
   int w = 0, head = 5 * (100000000);
   for (i = 0; i < len_float; i++) {
-    if (f & (1 << len_float - i - 1)) {
+  //  if (f & (1 << len_float - i - 1)) {
       w += head;
-    }
+//    }
     head /= 2;
   }
   len_float = sprintf(buf + len_int, "%d", w);
