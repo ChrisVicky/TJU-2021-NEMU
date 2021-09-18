7214eb700c67fa474fc9a0955a979065f8c85b27
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-18 17:20:25 +0000

    >  run 3020202184 simpleedu Linux simpleedu 4.15.0-154-generic #161-Ubuntu SMP Fri Jul 30 13:04:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:20:25 up 11 days,  9:12,  1 user,  load average: 0.05, 0.08, 0.03 6f07a46fb92cda3ba29a08f8ecfcb131f53872f

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -135,10 +135,9 @@ static void modify_ppfs_setargs() {
 	unsigned int ppfs_addr = &_ppfs_setargs;
 	unsigned int jmp_addr = ppfs_addr + (0x801157 - 0x8010e3);
 	unsigned int destination_addr = ppfs_addr + (0x801101 - 0x8010e3);
-	
-	int rel1 = (0x8010f1 - 0x801147) & 0xff;
 	int * jmp_pointer = (int *) jmp_addr;
-	*(jmp_pointer) = 0xeb8790 + (*(jmp_pointer) & 0xff000000);
+	unsigned int saved_bit = *(jmp_pointer) & 0xff000000;
+	*(jmp_pointer) = 0x9087eb + saved_bit;
 //	*(jmp_pointer) = *(jmp_pointer) & 0xff000000 + (rel1<<8) + 0xeb + (0x90 << 16);
   /* TODO: Implement this function to modify the action of preparing
    * "%f" arguments for _vfprintf_internal() in _ppfs_setargs().
