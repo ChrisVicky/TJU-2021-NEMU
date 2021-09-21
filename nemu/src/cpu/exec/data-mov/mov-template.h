33ce11a7a0aaa9730f8d14040b24693277abf6e7
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-21 23:51:24 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:51:24 up  5:02,  1 user,  load average: 0.98, 0.88, 0.85 2f00a7c35c3b710161c3ed4926d37ce56d212bb5

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -54,10 +54,10 @@ make_helper(concat(mov_cr2r_, SUFFIX)){
 make_helper(mov_sr2r_b){
 	ModR_M m;
 	m.val = instr_fetch(eip+1, 1);
-	Log("mov %x, %x" ,m.reg, m.R_M);
+	//Log("mov %x, %x" ,m.reg, m.R_M);
 	cpu.sreg[m.reg].visible.val = REG(m.R_M);
-	Log("cpu.sreg[%x].visible.val = %x" ,m.reg,REG(m.R_M));
-	Log("Now index = %x	RPL = %x	TI = %x" ,cpu.sreg[m.reg].visible.index, cpu.sreg[m.reg].visible.RPL, cpu.sreg[m.reg].visible.TI);
+	//Log("cpu.sreg[%x].visible.val = %x" ,m.reg,REG(m.R_M));
+	//Log("Now index = %x	RPL = %x	TI = %x" ,cpu.sreg[m.reg].visible.index, cpu.sreg[m.reg].visible.RPL, cpu.sreg[m.reg].visible.TI);
 	load_sreg(m.reg);
 	char *SN[] = {"es","cs","ss","ds"};
 	print_asm(str(instr) " %%%s, %%%s" ,REG_NAME(m.R_M),SN[m.reg]);
