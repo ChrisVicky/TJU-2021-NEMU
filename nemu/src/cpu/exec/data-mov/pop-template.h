d2df0182cf613a7c2d5849f726c3b73d12622765
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-21 17:43:05 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:43:05 up  5:42,  1 user,  load average: 0.78, 0.71, 0.79 7c2172397ea9af1449921f006e49c4302f476be5

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -3,8 +3,8 @@
 #define instr pop
 
 static void do_execute() {
-    OPERAND_W(op_src, MEM_R(REG(R_ESP), R_SS));
-    REG(R_ESP) += DATA_BYTE;
+    OPERAND_W(op_src, MEM_R(cpu.esp, R_SS));
+    cpu.esp += DATA_BYTE;
     print_asm_template1();
 }
 
