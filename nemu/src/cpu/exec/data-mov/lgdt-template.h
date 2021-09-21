5ad368d2a02adbc9cd2ec1b6e8a28a22b7c633b0
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-21 19:43:03 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:43:03 up 53 min,  1 user,  load average: 0.53, 0.51, 0.31 bf20a323677a57196dc41223bac0958ce1e27cee

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -10,7 +10,6 @@ make_helper(concat(lgdt_, SUFFIX)) {
 #endif
     cpu.GDTR.Base = base;
     cpu.GDTR.Limit = limit;
-    print_asm_template1();
     print_asm(str(instr) " 0x%04x, 0x%08x", limit, base);
     return 6;
 }
