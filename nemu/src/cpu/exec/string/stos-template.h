850bae915bdb2fac3b09b9e451359bde44c7167b
Christopher_Vi <1051666563@qq.com>
2021-09-22 01:53:05 +0800

    Some bugs with string are yet solved

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -3,17 +3,11 @@
 #define instr stos
 
 make_helper(concat(stos_, SUFFIX)) {
-    MEM_W(reg_l(R_EDI), REG(R_EAX), R_ES);
-    if (cpu.eflags.DF == 0)
-    {
-        reg_l(R_EDI) += DATA_BYTE;
-    } else {
-        reg_l(R_EDI) -= DATA_BYTE;
-    }
-    print_asm("stos%s", str(SUFFIX));
+	MEM_W(cpu.edi, REG(R_EAX), R_ES);
+	cpu.edi += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
 
-    return 1;
-    
+	print_asm("stos" str(SUFFIX) " %%%s,%%es:(%%edi)", REG_NAME(R_EAX));
+	return 1;
 }
 
 #include "cpu/exec/template-end.h"
