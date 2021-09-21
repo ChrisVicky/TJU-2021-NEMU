ee19b032cda9e8622cb89c74a14515ec1297d153
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-21 22:40:14 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:40:14 up  3:50,  1 user,  load average: 1.29, 0.98, 0.74 7e2b9453cde7c75dedacb1554533169881614a03

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -51,4 +51,5 @@ void load_sreg(uint8_t sreg_index){
 	cpu.sreg[sreg_index].invisible.cache.base = temp.base_15_0 | (temp.base_23_16<<16) | (temp.base_31_24<<24);
 	cpu.sreg[sreg_index].invisible.cache.limit = (temp.limit_19_16 << 16) | temp.limit_15_0;
 	if(temp.granularity) cpu.sreg[sreg_index].invisible.cache.limit = cpu.sreg[sreg_index].invisible.cache.limit<<12;
+	Log("base = %x limit = %x\n" ,cpu.sreg[sreg_index].invisible.cache.base,cpu.sreg[sreg_index].invisible.cache.limit);
 }
