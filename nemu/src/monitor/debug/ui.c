282547f1fdd0580001a4b81cfa2520b782476e8c
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-22 01:03:17 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:03:17 up  6:13,  1 user,  load average: 0.89, 0.91, 0.86 2be2493e3452fee5d1c819247ab4e875ef8e106

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -26,6 +26,7 @@ void ui_mainloop()
 		/* treat the remaining string as the arguments,
 		 * which may need further parsing
 		 */
+		Log("INSTR");
 		char *args = cmd + strlen(cmd) + 1;
 		if (args >= str_end)
 		{
