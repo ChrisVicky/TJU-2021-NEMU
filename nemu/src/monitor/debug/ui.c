a2d6d22c44443c7a26776c1cd23cfd31c4510193
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-22 00:58:40 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:58:40 up  6:09,  1 user,  load average: 0.73, 0.77, 0.81 ec992321ac910555c8bbae798c43b39cb29acf1

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -13,14 +13,7 @@ void ui_mainloop()
 	SUCCESS("DEBUG\n");
 	while (1)
 	{
-		/* Print out the SEGMENT_REG */
-		int ii = 0;
-		char *SN[] = {"ES","CS","SS","DS"};
-		Log("Current eip = %x" ,cpu.eip);
-		Log("%-10s%-10s%-10s" ,"Name","visible","limit");
-		for(ii=0;ii<4;ii++){
-			Log("%-10s%-10x%-16x" ,SN[ii],cpu.sreg[ii].visible.val,cpu.sreg[ii].invisible.cache.limit);
-		}
+		
 
 		int i;
 		char *str = rl_gets();
