1aa5a7b60664ef1ffb50ba91f17f6a1e97e9c030
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-22 00:49:46 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:49:46 up  6:00,  1 user,  load average: 0.75, 0.79, 0.80 e33b07da60ed0d36d6d9ee6beca9b374892111

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -49,8 +49,8 @@ void load_sreg(uint8_t sreg_index){
 	SegDesc temp;
 	temp.first_val = lnaddr_read(gdt, 4);
 	temp.second_val = lnaddr_read(gdt + 4, 4);
-	cpu.sreg[sreg_index].invisible.cache.base = temp.base_15_0 | (temp.base_23_16<<16) | (temp.base_31_24<<24);
-	uint32_t limit = (temp.limit_19_16 << 16) | temp.limit_15_0;
+	cpu.sreg[sreg_index].invisible.cache.base = ((uint32_t)temp.base_15_0) | (((uint32_t)temp.base_23_16)<<16) | (((uint32_t)temp.base_31_24)<<24);
+	uint32_t limit = (((uint32_t)temp.limit_19_16) << 16) | ((uint32_t)temp.limit_15_0);
 	if(temp.granularity) limit = (limit << 12);
 	cpu.sreg[sreg_index].invisible.cache.limit = limit;
 	Log("base = %x limit = %x\n" ,cpu.sreg[sreg_index].invisible.cache.base,cpu.sreg[sreg_index].invisible.cache.limit);
