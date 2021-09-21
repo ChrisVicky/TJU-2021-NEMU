448b1f9c7286a12aa1cea60fce86fe4637ae3702
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-22 01:07:54 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:07:54 up  6:18,  1 user,  load average: 1.01, 1.03, 0.93 7197b4cdc83b41b33631b8744d428ae34ff3a217

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -54,12 +54,12 @@ make_helper(jmp_rm_l) {
 }
 
 make_helper(ljmp_l) {
-	Log("fetch1");
 	op_src->val = instr_fetch(eip+1, 4);
 	op_src->type = OP_TYPE_IMM;
 	
 	Log("fetch2");
 	op_dest->val = instr_fetch(eip+5, 2);
+	Log("fetch 2 end");
 	op_dest->type = OP_TYPE_IMM;
 	cpu.sreg[R_CS].visible.val = op_dest->val;
 	cpu.sreg[R_CS].invisible.cache.base = 0;
