0e80f66fc120150fc206cd0011a6b5e49f9e0b24
Christopher_Vi <1051666563@qq.com>
2021-09-22 11:35:53 +0800

    Strcmp -> return 1 if not equal

--- nemu/src/cpu/exec/logic/xor-template.h
+++ nemu/src/cpu/exec/logic/xor-template.h
@@ -3,11 +3,10 @@
 #define instr xor
 
 static void do_execute () {
-//	DATA_TYPE result = op_dest->val ^ op_src->val;
-	uint32_t result = op_dest->val ^ op_src->val;
+	DATA_TYPE result = op_dest->val ^ op_src->val;
 	OPERAND_W(op_dest, result);
 
-	update_eflags_pf_zf_sf(result);
+	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
 	cpu.eflags.CF = cpu.eflags.OF = 0;
 
 	print_asm_template2();
