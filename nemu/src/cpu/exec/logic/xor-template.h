012fb6639d39b11308e595d59d772d476f281eb1
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-22 11:20:56 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:20:56 up  1:08,  1 user,  load average: 0.80, 0.75, 0.86 4fe0332f6ffb4f0c91b455b1ae3c777b52d88eb8

--- nemu/src/cpu/exec/logic/xor-template.h
+++ nemu/src/cpu/exec/logic/xor-template.h
@@ -3,10 +3,11 @@
 #define instr xor
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val ^ op_src->val;
+//	DATA_TYPE result = op_dest->val ^ op_src->val;
+	uint32_t result = op_dest->val ^ op_src->val;
 	OPERAND_W(op_dest, result);
 
-	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	update_eflags_pf_zf_sf(result);
 	cpu.eflags.CF = cpu.eflags.OF = 0;
 
 	print_asm_template2();
