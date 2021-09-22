de712ee51683ff2e85d613f2b6e136e1608eb1ae
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-22 11:43:39 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:43:39 up  1:31,  1 user,  load average: 0.88, 0.76, 0.70 c1407d90a05dc099430c734abb2f6f71c8f0e2a0

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -3,38 +3,15 @@
 #define instr sbb
 
 static void do_execute () {
-	    DATA_TYPE src = op_src->val;
-    if (op_src->size == 1 && op_dest->size !=1)
-    {
-        op_src->val = (int8_t)op_src->val;
-    }
-    src += cpu.eflags.CF;
-    DATA_TYPE result = op_dest->val - src;
-    int len = (DATA_BYTE << 3) - 1;
-    cpu.eflags.CF = op_dest->val < src;
-    int s1, s2;
-    cpu.eflags.SF = result >> len;
-    s1 = op_dest->val >> len;
-    s2 = src >> len;
-    cpu.eflags.OF = (s1!=s2 && s2==cpu.eflags.SF);
-    cpu.eflags.ZF = !result;
-    OPERAND_W(op_dest, result);
-    result ^= result >> 4;
-    result ^= result >> 2;
-    result ^= result >> 1;
-    cpu.eflags.PF = !(result&1);
-    print_asm_template2();
-	
-	// DATA_TYPE result = op_dest->val - (op_src->val + cpu.eflags.CF);
-	// OPERAND_W(op_dest, result);
+	DATA_TYPE result = op_dest->val - (op_src->val + cpu.eflags.CF);
+	OPERAND_W(op_dest, result);
 
-	// /* TODO: Update EFLAGS. */
-	// update_eflags_pf_zf_sf((DATA_TYPE_S)result);
-	// cpu.eflags.CF = result < op_dest->val;
-	// cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+	/* TODO: Update EFLAGS. */
+	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = result < op_dest->val;
+	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
 
-	// print_asm_template2();
-	
+	print_asm_template2();
 }
 
 make_instr_helper(r2rm)
