9f497ad581680d54dd2329f8d61525836941fe48
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-22 15:35:59 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:35:59 up 24 min,  1 user,  load average: 1.69, 1.38, 0.80 5218f9001e2c25837ea6d74741c14f0fd50d01b

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -66,7 +66,8 @@ void cpu_exec(volatile uint32_t n) {
 		int instr_len = exec(cpu.eip);
 
 		cpu.eip += instr_len;
-		Log("CF: %x" ,cpu.eflags.CF);
+		if(cpu.eflags.CF)
+			Log("CF: %x" ,cpu.eflags.CF);
 
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
