093fbb02feb9c222f887eea509f0756435754668
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-28 16:12:54 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:12:54 up 54 min,  1 user,  load average: 0.68, 0.88, 0.78 f51088c84ee6814c77d81e843ea3f402bda53a63

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,6 +3,7 @@
 #define instr movs
 
 make_helper(concat(movs_n_, SUFFIX)) {
+    Log("addr: %x " ,reg_l(R_ESI) );
     swaddr_write(reg_l(R_EDI), DATA_BYTE, swaddr_read(reg_l(R_ESI), DATA_BYTE, R_DS), R_ES);
 
     if (cpu.eflags.DF == 0) reg_l(R_EDI) += DATA_BYTE, reg_l(R_ESI) += DATA_BYTE;
