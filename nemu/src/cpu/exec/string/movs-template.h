8263d8c5f5646491db6a1b10843cdb5e14357d40
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-28 15:53:06 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:53:06 up 34 min,  1 user,  load average: 0.81, 0.89, 0.82 663542a0897b828af85288e0d4ebd8f3d0420a8d

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -2,13 +2,28 @@
 
 #define instr movs
 
-make_helper(concat(movs_n_, SUFFIX)) {
-    swaddr_write(reg_l(R_EDI), DATA_BYTE, swaddr_read(reg_l(R_ESI), DATA_BYTE, R_DS), R_ES);
-    if (cpu.eflags.DF == 0) reg_l(R_EDI) += DATA_BYTE, reg_l(R_ESI) += DATA_BYTE;
-    else reg_l(R_EDI) -= DATA_BYTE, reg_l(R_ESI) -=DATA_BYTE;
+// make_helper(concat(movs_n_, SUFFIX)) {
+//     swaddr_write(reg_l(R_EDI), DATA_BYTE, swaddr_read(reg_l(R_ESI), DATA_BYTE, R_DS), R_ES);
+//     if (cpu.eflags.DF == 0) reg_l(R_EDI) += DATA_BYTE, reg_l(R_ESI) += DATA_BYTE;
+//     else reg_l(R_EDI) -= DATA_BYTE, reg_l(R_ESI) -=DATA_BYTE;
     
-    print_asm("movs");
-    return 1;
+//     print_asm("movs");
+//     return 1;
+// }
+make_helper(concat(movs_n_, SUFFIX)) {
+  uint32_t tmp = MEM_R(reg_l(R_ESI), R_DS);
+  swaddr_write(reg_l(R_EDI), DATA_BYTE, tmp, R_ES);
+
+  if (!cpu.eflags.DF) {
+    reg_l(R_EDI) += DATA_BYTE;
+    reg_l(R_ESI) += DATA_BYTE;
+  } else {
+    reg_l(R_EDI) -= DATA_BYTE;
+    reg_l(R_ESI) -= DATA_BYTE;
+  }
+  print_asm("movs");
+  return 1;
 }
+
 
 #include "cpu/exec/template-end.h"
