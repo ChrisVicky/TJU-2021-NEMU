436ca57c6da392ecf650f211fef971d08adead6c
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-28 16:08:40 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:08:40 up 49 min,  1 user,  load average: 1.62, 0.92, 0.75 367d94ad7681fc44a8c6c99357939efadd3dfc

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -14,6 +14,7 @@ make_helper(rep) {
 		while(cpu.ecx) {
 			exec(eip + 1);
 			count ++;
+			Log("incount: %x" ,count);
 			cpu.ecx --;
 			assert(ops_decoded.opcode == 0xa4	// movsb
 				|| ops_decoded.opcode == 0xa5	// movsw
