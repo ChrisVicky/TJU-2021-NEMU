23ab43d322590a3232c4ca0ff022ce172aa557ae
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-28 18:23:08 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:23:08 up 21 min,  1 user,  load average: 0.89, 0.64, 0.60 4a369e2432401a3efd2426c32823665975be33f5

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,7 +3,9 @@
 #define instr movs
 
 make_helper(concat(movs_n_, SUFFIX)) {
-    swaddr_write(reg_l(R_EDI), DATA_BYTE, swaddr_read(reg_l(R_ESI), DATA_BYTE, R_DS), R_ES);
+    uint32_t data = swaddr_read(reg_l(R_ESI), DATA_BYTE, R_DS);
+    Log("data: %x" ,data);
+    swaddr_write(reg_l(R_EDI), DATA_BYTE, data, R_ES);
 
     if (cpu.eflags.DF == 0) reg_l(R_EDI) += DATA_BYTE, reg_l(R_ESI) += DATA_BYTE;
     else reg_l(R_EDI) -= DATA_BYTE, reg_l(R_ESI) -=DATA_BYTE;
