253af627847b678c2c978e19061192d179c9b077
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-30 13:49:32 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:49:32 up  1:31,  1 user,  load average: 0.62, 0.41, 0.47 3df6b7d81658856be92b99409dfea1e6fea293f

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -14,13 +14,14 @@ void video_mapping_write_test();
 void video_mapping_read_test();
 void video_mapping_clear();
 
-void init_cond();
 volatile int x = 0;
+void init_cond();
 /* Initialization phase 1
  * The assembly code in start.S will finally jump here.
  */
 void init() {
 #ifdef IA32_PAGE
+	x  =  1;
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
 	 * Before setting up correct paging, no global variable can be used. */
