4fd065185229a115af7d1e8967a1fd76a59ca6dd
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-30 18:03:25 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:03:25 up 5 min,  1 user,  load average: 0.86, 0.80, 0.39 2d4b3582f90ce7ce89f90fd9dca38e4d6d3b7

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -33,8 +33,7 @@ void init() {
 #endif
 
 	/* Jump to init_cond() to continue initialization. */
-//	asm volatile("jmp *%0" : : "r"(init_cond));
-	init_cond();
+	asm volatile("jmp *%0" : : "r"(init_cond));
 	panic("should not reach here");
 }
 
