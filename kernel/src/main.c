126aba3edc52cd50957bc46485cb5af69c5f834c
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-30 13:31:06 +0800

    >  compile NEMU 3020202184 christopher Linux chricky 5.11.0-34-generic #36~20.04.1-Ubuntu SMP Fri Aug 27 08:06:32 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:31:06 up  1:12,  1 user,  load average: 0.84, 0.83, 0.71 fbdf3cfffc53779185b9288eb05ba81aaa61ceb0

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -15,7 +15,7 @@ void video_mapping_read_test();
 void video_mapping_clear();
 
 void init_cond();
-
+volatile int x = 0;
 /* Initialization phase 1
  * The assembly code in start.S will finally jump here.
  */
